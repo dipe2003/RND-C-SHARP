716fe30ed2c3b3a40a151ba2e52cfc76a055a020
Diego Perez <dipe.2003@hotmail.com>
2017-09-18 19:17:15 -0300

    Se corrigio error que no permitia realizar sorteos de fecha. Se quitaron sorteos de Dia y Mes en Sorteo de numeros. Se agregaron sorteos predefinidos para Dia de la semana y mes del año. Se agregaron enumeraciones para dias y meses en español.

---
+++ RND/Clases/EnumDiaSpanish.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RND.Clases {
+    enum EnumDiaSpanish{
+        DOMINGO,
+        LUNES,
+        MARTES,
+        MIERCOLES,
+        JUEVES,
+        VIERNES,
+        SABADO
+    }
+}
