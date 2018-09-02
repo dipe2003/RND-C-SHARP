f509dac95976f304b99e94b172d8e6552264fb6e
Diego Perez <dipe.2003@hotmail.com>
2018-09-02 18:13:30 -0300

    Se ajustó el contenido y la ubicación de la imágen de los formularios de ayuda para que al mostrar el menú lateral no quede el texto oculto.

--- RND/Views/Ayuda/AyudaSorteoFechas.Designer.cs
+++ RND/Views/Ayuda/AyudaSorteoFechas.Designer.cs
@@ -49,11 +49,11 @@
             // 
             this.richTextBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.richTextBox4.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox4.Location = new System.Drawing.Point(167, 144);
+            this.richTextBox4.Location = new System.Drawing.Point(13, 152);
             this.richTextBox4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.richTextBox4.Name = "richTextBox4";
             this.richTextBox4.ReadOnly = true;
-            this.richTextBox4.Size = new System.Drawing.Size(223, 36);
+            this.richTextBox4.Size = new System.Drawing.Size(207, 36);
             this.richTextBox4.TabIndex = 25;
             this.richTextBox4.Text = "Cantidad de fechas aleatorios a obtener.";
             // 
@@ -61,11 +61,11 @@
             // 
             this.richTextBox7.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.richTextBox7.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox7.Location = new System.Drawing.Point(399, 211);
+            this.richTextBox7.Location = new System.Drawing.Point(230, 219);
             this.richTextBox7.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.richTextBox7.Name = "richTextBox7";
             this.richTextBox7.ReadOnly = true;
-            this.richTextBox7.Size = new System.Drawing.Size(223, 36);
+            this.richTextBox7.Size = new System.Drawing.Size(207, 36);
             this.richTextBox7.TabIndex = 19;
             this.richTextBox7.Text = "No comprueba que las fechas generadas sean únicas.";
             // 
@@ -73,11 +73,11 @@
             // 
             this.richTextBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.richTextBox6.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox6.Location = new System.Drawing.Point(167, 211);
+            this.richTextBox6.Location = new System.Drawing.Point(13, 219);
             this.richTextBox6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.richTextBox6.Name = "richTextBox6";
             this.richTextBox6.ReadOnly = true;
-            this.richTextBox6.Size = new System.Drawing.Size(223, 36);
+            this.richTextBox6.Size = new System.Drawing.Size(207, 36);
             this.richTextBox6.TabIndex = 18;
             this.richTextBox6.Text = "Ordena de forma ascendente las fechas generadas.";
             // 
@@ -85,7 +85,7 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(396, 191);
+            this.label8.Location = new System.Drawing.Point(227, 199);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(135, 16);
             this.label8.TabIndex = 16;
@@ -95,7 +95,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(164, 191);
+            this.label7.Location = new System.Drawing.Point(10, 199);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(63, 16);
             this.label7.TabIndex = 15;
@@ -105,11 +105,11 @@
             // 
             this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.richTextBox2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox2.Location = new System.Drawing.Point(399, 76);
+            this.richTextBox2.Location = new System.Drawing.Point(230, 84);
             this.richTextBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.richTextBox2.Name = "richTextBox2";
             this.richTextBox2.ReadOnly = true;
-            this.richTextBox2.Size = new System.Drawing.Size(223, 36);
+            this.richTextBox2.Size = new System.Drawing.Size(207, 36);
             this.richTextBox2.TabIndex = 24;
             this.richTextBox2.Text = "Fecha en que finaliza el sorteo.";
             // 
@@ -117,7 +117,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(164, 124);
+            this.label4.Location = new System.Drawing.Point(10, 132);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(70, 16);
             this.label4.TabIndex = 11;
@@ -127,7 +127,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(396, 56);
+            this.label3.Location = new System.Drawing.Point(227, 64);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(82, 16);
             this.label3.TabIndex = 10;
@@ -137,7 +137,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(164, 56);
+            this.label2.Location = new System.Drawing.Point(10, 64);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(87, 16);
             this.label2.TabIndex = 9;
@@ -147,11 +147,11 @@
             // 
             this.richTextBox8.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.richTextBox8.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox8.Location = new System.Drawing.Point(169, 299);
+            this.richTextBox8.Location = new System.Drawing.Point(13, 299);
             this.richTextBox8.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.richTextBox8.Name = "richTextBox8";
             this.richTextBox8.ReadOnly = true;
-            this.richTextBox8.Size = new System.Drawing.Size(453, 88);
+            this.richTextBox8.Size = new System.Drawing.Size(424, 88);
             this.richTextBox8.TabIndex = 21;
             this.richTextBox8.Text = resources.GetString("richTextBox8.Text");
             // 
@@ -159,7 +159,7 @@
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(151, 277);
+            this.label9.Location = new System.Drawing.Point(6, 277);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(155, 18);
             this.label9.TabIndex = 7;
@@ -169,11 +169,11 @@
             // 
             this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.richTextBox1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox1.Location = new System.Drawing.Point(167, 76);
+            this.richTextBox1.Location = new System.Drawing.Point(13, 84);
             this.richTextBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.ReadOnly = true;
-            this.richTextBox1.Size = new System.Drawing.Size(223, 36);
+            this.richTextBox1.Size = new System.Drawing.Size(207, 36);
             this.richTextBox1.TabIndex = 23;
             this.richTextBox1.Text = "Fecha desde el que inicia el sorteo.";
             // 
@@ -181,7 +181,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(151, 30);
+            this.label1.Location = new System.Drawing.Point(6, 30);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(80, 18);
             this.label1.TabIndex = 13;
@@ -191,10 +191,10 @@
             // 
             this.pictureBoxSorteoNumeros.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.pictureBoxSorteoNumeros.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxSorteoNumeros.Image")));
-            this.pictureBoxSorteoNumeros.Location = new System.Drawing.Point(23, 30);
+            this.pictureBoxSorteoNumeros.Location = new System.Drawing.Point(473, 30);
             this.pictureBoxSorteoNumeros.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBoxSorteoNumeros.Name = "pictureBoxSorteoNumeros";
-            this.pictureBoxSorteoNumeros.Size = new System.Drawing.Size(122, 475);
+            this.pictureBoxSorteoNumeros.Size = new System.Drawing.Size(140, 475);
             this.pictureBoxSorteoNumeros.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBoxSorteoNumeros.TabIndex = 6;
             this.pictureBoxSorteoNumeros.TabStop = false;
@@ -203,7 +203,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(396, 124);
+            this.label6.Location = new System.Drawing.Point(227, 132);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(117, 16);
             this.label6.TabIndex = 14;
@@ -213,11 +213,11 @@
             // 
             this.richTextBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.richTextBox3.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox3.Location = new System.Drawing.Point(399, 144);
+            this.richTextBox3.Location = new System.Drawing.Point(230, 152);
             this.richTextBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.richTextBox3.Name = "richTextBox3";
             this.richTextBox3.ReadOnly = true;
-            this.richTextBox3.Size = new System.Drawing.Size(223, 36);
+            this.richTextBox3.Size = new System.Drawing.Size(207, 36);
             this.richTextBox3.TabIndex = 17;
             this.richTextBox3.Text = "Considera los domingos como días válidos para sortear.";
             // 
@@ -225,7 +225,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(166, 399);
+            this.label5.Location = new System.Drawing.Point(10, 399);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(119, 16);
             this.label5.TabIndex = 15;
@@ -235,11 +235,11 @@
             // 
             this.richTextBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.richTextBox5.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox5.Location = new System.Drawing.Point(169, 419);
+            this.richTextBox5.Location = new System.Drawing.Point(13, 419);
             this.richTextBox5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.richTextBox5.Name = "richTextBox5";
             this.richTextBox5.ReadOnly = true;
-            this.richTextBox5.Size = new System.Drawing.Size(453, 86);
+            this.richTextBox5.Size = new System.Drawing.Size(424, 86);
             this.richTextBox5.TabIndex = 18;
             this.richTextBox5.Text = resources.GetString("richTextBox5.Text");
             // 
